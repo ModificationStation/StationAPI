400b327dc7b2dd72f89012ea1dfe7f5e584454b7
mineLdiver <aabesedin@mail.ru>
2020-08-27 18:13:42 +0500

    EnumFactory added, ToolMaterial factory in GeneralFactory added

---
+++ src/main/java/net/modificationstation/stationloader/api/common/factory/EnumFactory.java
@@ -0,0 +1,26 @@
+package net.modificationstation.stationloader.api.common.factory;
+
+import net.modificationstation.stationloader.api.common.util.HasHandler;
+
+public interface EnumFactory extends HasHandler<EnumFactory> {
+
+    EnumFactory INSTANCE = new EnumFactory() {
+
+        private EnumFactory handler;
+
+        @Override
+        public void setHandler(EnumFactory handler) {
+            this.handler = handler;
+        }
+
+        @Override
+        public <T extends Enum<?>> T addEnum(Class<T> enumType, String enumName, Class<?>[] paramTypes, Object[] paramValues) {
+            if (handler == null)
+                throw new RuntimeException("Accessed StationLoader too early!");
+            else
+                return handler.addEnum(enumType, enumName, paramTypes, paramValues);
+        }
+    };
+
+    <T extends Enum<? >> T addEnum(Class<T> enumType, String enumName, Class<?>[] paramTypes, Object[] paramValues);
+}
