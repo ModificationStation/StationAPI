ad15ad62158e8397724c3bb5f4fe75a5cbd5dee5
calmilamsy <calmilamsy@pymcl.net>
2020-10-05 03:08:42 +0100

    Fix some dumb code
    I completely forgot Inject existed for some reason.

--- src/main/java/net/modificationstation/stationloader/mixin/common/MixinTileEntityBase.java
+++ src/main/java/net/modificationstation/stationloader/mixin/common/MixinTileEntityBase.java
@@ -1,12 +1,12 @@
 package net.modificationstation.stationloader.mixin.common;
 
 import net.minecraft.tileentity.TileEntityBase;
-import net.minecraft.tileentity.TileEntityPiston;
 import net.modificationstation.stationloader.api.common.event.block.TileEntityRegister;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.HashMap;
 
@@ -18,15 +18,12 @@ public class MixinTileEntityBase {
     }
 
     @SuppressWarnings("UnresolvedMixinReference")
-    @Redirect(method="<clinit>", at=@At(target = "Lnet/minecraft/tileentity/TileEntityBase;register(Ljava/lang/Class;Ljava/lang/String;)V", value = "INVOKE"))
-    private static void registerModdedTileEntities(Class tileEntity, String id) {
-        register(tileEntity, id);
-        if (tileEntity == TileEntityPiston.class) {
-            HashMap<String, Class> moddedTileEntities = new HashMap<>();
-            TileEntityRegister.EVENT.getInvoker().registerTileEntities(moddedTileEntities);
-            for (String moddedTileEntity : moddedTileEntities.keySet()) {
-                register(moddedTileEntities.get(moddedTileEntity), moddedTileEntity);
-            }
+    @Inject(method="<clinit>", at=@At(value = "TAIL"))
+    private static void registerModdedTileEntities(CallbackInfo ci) {
+        HashMap<String, Class> moddedTileEntities = new HashMap<>();
+        TileEntityRegister.EVENT.getInvoker().registerTileEntities(moddedTileEntities);
+        for (String moddedTileEntity : moddedTileEntities.keySet()) {
+            register(moddedTileEntities.get(moddedTileEntity), moddedTileEntity);
         }
     }
 }
