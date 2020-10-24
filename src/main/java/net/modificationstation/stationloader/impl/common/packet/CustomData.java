bb56142af6fe1bf55712f89ce523b0c1827be213
mineLdiver <aabesedin@mail.ru>
2020-10-24 15:27:28 +0500

    CustomData writing

--- src/main/java/net/modificationstation/stationloader/impl/common/packet/CustomData.java
+++ src/main/java/net/modificationstation/stationloader/impl/common/packet/CustomData.java
@@ -7,6 +7,7 @@ import net.modificationstation.stationloader.api.common.registry.ModIDRegistry;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 
 public class CustomData extends AbstractPacket {
 
@@ -14,7 +15,7 @@ public class CustomData extends AbstractPacket {
 
     public CustomData(ModMetadata data, String packetID) {
         this.modid = data.getId();
-        this.packetID = packetID;
+        this.packetid = packetID;
     }
 
     @Override
@@ -23,33 +24,124 @@ public class CustomData extends AbstractPacket {
 
     @Override
     public void write(DataOutputStream out) {
-
+        try {
+            out.writeInt(modid.length());
+            out.writeChars(modid);
+            out.writeInt(packetid.length());
+            out.writeChars(packetid);
+            boolean[] absent = new boolean[] {
+                    booleans == null,
+                    bytes == null,
+                    shorts == null,
+                    chars == null,
+                    ints == null,
+                    longs == null,
+                    floats == null,
+                    doubles == null,
+                    strings == null
+            };
+            out.writeShort((short) ((absent[0] ? 0 : 256) +
+                    (absent[1] ? 0 : 128) +
+                    (absent[2] ? 0 : 64) +
+                    (absent[3] ? 0 : 32) +
+                    (absent[4] ? 0 : 16) +
+                    (absent[5] ? 0 : 8) +
+                    (absent[6] ? 0 : 4) +
+                    (absent[7] ? 0 : 2) +
+                    (absent[8] ? 0 : 1)));
+            if (!absent[0]) {
+                int length = booleans.length;
+                out.writeInt(length);
+                int i0, i1, i2, i3, i4, i5, i6, i7;
+                for (int i = 0; i < (int) Math.ceil((double) length / 8); i++) {
+                    i0 = i * 8;
+                    i1 = i * 8 + 1;
+                    i2 = i * 8 + 2;
+                    i3 = i * 8 + 3;
+                    i4 = i * 8 + 4;
+                    i5 = i * 8 + 5;
+                    i6 = i * 8 + 6;
+                    i7 = i * 8 + 7;
+                    out.writeByte((byte) ((length > i0 && booleans[i0] ? 128 : 0) +
+                            (length > i1 && booleans[i1] ? 64 : 0) +
+                            (length > i2 && booleans[i2] ? 32 : 0) +
+                            (length > i3 && booleans[i3] ? 16 : 0) +
+                            (length > i4 && booleans[i4] ? 8 : 0) +
+                            (length > i5 && booleans[i5] ? 4 : 0) +
+                            (length > i6 && booleans[i6] ? 2 : 0) +
+                            (length > i7 && booleans[i7] ? 1 : 0)));
+                }
+            }
+            if (!absent[1]) {
+                out.writeInt(bytes.length);
+                for (byte b : bytes)
+                    out.writeByte(b);
+            }
+            if (!absent[2]) {
+                out.writeInt(shorts.length);
+                for (short s : shorts)
+                    out.writeShort(s);
+            }
+            if (!absent[3]) {
+                out.writeInt(chars.length);
+                for (char c : chars)
+                    out.writeChar(c);
+            }
+            if (!absent[4]) {
+                out.writeInt(ints.length);
+                for (int i : ints)
+                    out.writeInt(i);
+            }
+            if (!absent[5]) {
+                out.writeInt(ints.length);
+                for (long l : longs)
+                    out.writeLong(l);
+            }
+            if (!absent[6]) {
+                out.writeInt(floats.length);
+                for (float f : floats)
+                    out.writeFloat(f);
+            }
+            if (!absent[7]) {
+                out.writeInt(doubles.length);
+                for (double d : doubles)
+                    out.writeDouble(d);
+            }
+            if (!absent[8]) {
+                out.writeInt(strings.length);
+                for (String s : strings) {
+                    out.writeInt(s.length());
+                    out.writeChars(s);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void handle(PacketHandler handler) {
-        ModIDRegistry.packet.get(modid).get(packetID).accept(this);
+        ModIDRegistry.packet.get(modid).get(packetid).accept(this);
     }
 
     @Override
     public int length() {
-        return
-                size(modid.toCharArray()) +
-                        size(packetID.toCharArray()) +
-                        Short.BYTES +
-                        (booleans == null ? 0 : size(booleans)) +
-                        (bytes == null ? 0 : size(bytes)) +
-                        (shorts == null ? 0 : size(shorts)) +
-                        (chars == null ? 0 : size(chars)) +
-                        (ints == null ? 0 : size(ints)) +
-                        (longs == null ? 0 : size(longs)) +
-                        (floats == null ? 0 : size(floats)) +
-                        (doubles == null ? 0 : size(doubles)) +
-                        (strings == null ? 0 : size(strings));
+        return size(modid.toCharArray()) +
+                size(packetid.toCharArray()) +
+                Short.BYTES +
+                (booleans == null ? 0 : size(booleans)) +
+                (bytes == null ? 0 : size(bytes)) +
+                (shorts == null ? 0 : size(shorts)) +
+                (chars == null ? 0 : size(chars)) +
+                (ints == null ? 0 : size(ints)) +
+                (longs == null ? 0 : size(longs)) +
+                (floats == null ? 0 : size(floats)) +
+                (doubles == null ? 0 : size(doubles)) +
+                (strings == null ? 0 : size(strings));
     }
 
     public static int size(boolean[] booleans) {
-        return (int) (Integer.BYTES + Math.ceil((double) booleans.length / 8));
+        return Integer.BYTES + ((int) Math.ceil((double) booleans.length / 8));
     }
 
     public static int size(byte[] bytes) {
@@ -88,9 +180,7 @@ public class CustomData extends AbstractPacket {
     }
 
     private String modid;
-    private String packetID;
-
-    private short present;
+    private String packetid;
 
     public boolean[] booleans;
     public byte[] bytes;
