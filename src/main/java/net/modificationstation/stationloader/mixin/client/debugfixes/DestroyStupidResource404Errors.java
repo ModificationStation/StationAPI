ede2118981d787ab3b98454252f872d2c8b0c4be
calmilamsy <bumbill00@gmail.com>
2020-11-06 15:11:03 +0000

    Edited sound debug fix to enable sounds.
    Should make testing mods easier.

--- src/main/java/net/modificationstation/stationloader/mixin/client/debugfixes/DestroyStupidResource404Errors.java
+++ src/main/java/net/modificationstation/stationloader/mixin/client/debugfixes/DestroyStupidResource404Errors.java
@@ -1,19 +1,15 @@
 package net.modificationstation.stationloader.mixin.client.debugfixes;
 
-import net.fabricmc.loader.FabricLoader;
 import net.minecraft.client.util.ThreadDownloadResources;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(value = ThreadDownloadResources.class, priority = 999999)
 public class DestroyStupidResource404Errors {
 
-    @Inject(method = "run", at = @At(value = "HEAD"), remap = false, cancellable = true, require = 0)
-    public void run(CallbackInfo ci) {
-        if (FabricLoader.INSTANCE.isDevelopmentEnvironment()) {
-            ci.cancel();
-        }
+    @ModifyConstant(method = "run", constant = @Constant(stringValue = "http://s3.amazonaws.com/MinecraftResources/"), remap = false, require = 0)
+    public String run(String string) {
+        return "https://resourceproxy.pymcl.net/MinecraftResources/";
     }
 }
