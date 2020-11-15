e058750bcc1f91e7e5983312fcae18bd6baca592
calmilamsy <bumbill00@gmail.com>
2020-11-15 02:24:19 +0000

    Add UseFirst for items
    Works exactly like the one in forge.
    Return true to prevent normal interactions.
    Useful for things like wrenches and buckets being clicked on blocks with GUIs.

---
+++ src/main/java/net/modificationstation/stationloader/api/common/event/item/UseFirst.java
@@ -0,0 +1,9 @@
+package net.modificationstation.stationloader.api.common.event.item;
+
+import net.minecraft.entity.player.PlayerBase;
+import net.minecraft.item.ItemInstance;
+import net.minecraft.level.Level;
+
+public interface UseFirst {
+    public boolean onItemUseFirst(ItemInstance itemInstance, PlayerBase playerBase, Level level, int x, int y, int z, int sideClicked);
+}
