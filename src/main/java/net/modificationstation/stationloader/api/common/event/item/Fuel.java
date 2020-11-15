1d150cf35b1334da6766cf7c3d4bb401bb3f7044
calmilamsy <bumbill00@gmail.com>
2020-11-15 18:24:38 +0000

    Actually add the Fuel interface

---
+++ src/main/java/net/modificationstation/stationloader/api/common/event/item/Fuel.java
@@ -0,0 +1,8 @@
+package net.modificationstation.stationloader.api.common.event.item;
+
+import net.minecraft.item.ItemInstance;
+
+public interface Fuel {
+
+    int getFuelTime(ItemInstance itemInstance);
+}
