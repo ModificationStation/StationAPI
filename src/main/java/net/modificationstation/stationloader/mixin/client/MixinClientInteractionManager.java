cb90531b6588b15e17327ebac20889bad57fd38d
calmilamsy <bumbill00@gmail.com>
2020-11-15 02:28:54 +0000

    Fix always cancelling interactions

--- src/main/java/net/modificationstation/stationloader/mixin/client/MixinClientInteractionManager.java
+++ src/main/java/net/modificationstation/stationloader/mixin/client/MixinClientInteractionManager.java
@@ -16,7 +16,9 @@ public class MixinClientInteractionManager {
     @Inject(method = "method_1713(Lnet/minecraft/entity/player/PlayerBase;Lnet/minecraft/level/Level;Lnet/minecraft/item/ItemInstance;IIII)Z", at = @At(value = "HEAD"), cancellable = true)
     private void injectOnPlaceBlock(PlayerBase playerBase, Level level, ItemInstance itemInstance, int i, int j, int k, int i1, CallbackInfoReturnable<Boolean> cir) {
         if (itemInstance != null && itemInstance.getType() instanceof UseFirst) {
-            cir.setReturnValue(((UseFirst) itemInstance.getType()).onItemUseFirst(itemInstance, playerBase, level, i, j, k, i1));
+            if (((UseFirst) itemInstance.getType()).onItemUseFirst(itemInstance, playerBase, level, i, j, k, i1)) {
+                cir.setReturnValue(true);
+            }
         }
     }
 }
