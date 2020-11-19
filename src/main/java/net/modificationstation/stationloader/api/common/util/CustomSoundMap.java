1f04b1ff53a14a37e4960ae428d87d4b98d20bc6
calmilamsy <bumbill00@gmail.com>
2020-11-19 02:07:25 +0000

    Custom audio loader
    Namespaced and automatic.

---
+++ src/main/java/net/modificationstation/stationloader/api/common/util/CustomSoundMap.java
@@ -0,0 +1,9 @@
+package net.modificationstation.stationloader.api.common.util;
+
+import net.minecraft.class_267;
+
+import java.net.URL;
+
+public interface CustomSoundMap {
+    class_267 putSound(String id, URL url);
+}
---
+++ src/main/java/net/modificationstation/stationloader/mixin/client/MixinSoundMap.java
@@ -0,0 +1,40 @@
+package net.modificationstation.stationloader.mixin.client;
+
+import net.minecraft.class_267;
+import net.minecraft.util.SoundMap;
+import net.modificationstation.stationloader.api.common.util.CustomSoundMap;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Mixin(SoundMap.class)
+public class MixinSoundMap implements CustomSoundMap {
+    @Shadow private Map field_1089;
+
+    @Shadow private List field_1090;
+
+    @Shadow public int field_1086;
+
+    @Override
+    public class_267 putSound(String id, URL url) {
+        if (id.length() - id.replace(".", "").length() != 1) {
+            throw new RuntimeException("You MUST name your audio files with an extension, and with no extra dots or any spaces!\ne.g: \"wolf_bark.ogg\" is fine, but \"wolf_bark\", \"wolf.bark.ogg\" and \"wolf bark.ogg\" are not.");
+        }
+        String filename = id;
+        id = id.split("\\.")[0].toLowerCase();
+        id = id.replaceAll("/", ".");
+        if (!this.field_1089.containsKey(id)) {
+            this.field_1089.put(id, new ArrayList());
+        }
+        class_267 var4 = new class_267(filename, url);
+
+        ((List)this.field_1089.get(id)).add(var4);
+        this.field_1090.add(var4);
+        ++this.field_1086;
+        return var4;
+    }
+}
