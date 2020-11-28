462ba6508161048b8192058fee4691d060021164
calmilamsy <bumbill00@gmail.com>
2020-11-28 02:07:08 +0000

    Add a method for doing cursed things with colours

--- src/main/java/net/modificationstation/stationloader/api/client/gui/Colors.java
+++ src/main/java/net/modificationstation/stationloader/api/client/gui/Colors.java
@@ -20,4 +20,25 @@ public class Colors {
     public static final String LIGHT_PURPLE = "§d";
     public static final String YELLOW = "§e";
     public static final String WHITE = "§f";
+
+    public static String[] toArray() {
+        return new String[] {
+                "§0",
+                "§1",
+                "§2",
+                "§3",
+                "§4",
+                "§5",
+                "§6",
+                "§7",
+                "§8",
+                "§9",
+                "§a",
+                "§b",
+                "§c",
+                "§d",
+                "§e",
+                "§f"
+        };
+    }
 }
