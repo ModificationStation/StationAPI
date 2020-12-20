600d5553e85ebbbc45aaf47c602849ed4570447f
calmilamsy <bumbill00@gmail.com>
2020-12-20 01:31:05 +0000

    Add custom shear support
    Enjoy my hacky but functional workaround.

---
+++ src/main/java/net/modificationstation/stationloader/api/common/item/tool/Shear.java
@@ -0,0 +1,9 @@
+package net.modificationstation.stationloader.api.common.item.tool;
+
+/**
+ * If you want custom behaviours, see UseOnEntityFirst
+ * @see net.modificationstation.stationloader.api.common.item.UseOnEntityFirst
+ */
+public interface Shear extends ToolLevel {
+
+}
