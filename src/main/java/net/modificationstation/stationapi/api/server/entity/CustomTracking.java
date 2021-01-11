5a4737716dbc247fd21670788ecf58b6008b175b
mineLdiver <aabesedin@mail.ru>
2021-01-12 01:21:06 +0500

    Some corrections to Javadoc.

--- src/main/java/net/modificationstation/stationapi/api/server/entity/CustomTracking.java
+++ src/main/java/net/modificationstation/stationapi/api/server/entity/CustomTracking.java
@@ -19,11 +19,12 @@ public interface CustomTracking {
      * Track method that gets called after server tries tracking an entity by checking if it's instance of a vanilla class.
      * @param entityTracker current dimension's tracker instance. Can be used to (un)track entities.
      * @param trackedEntities the set of tracked entities. Can be used to check if entity is already tracked.
+     * @see Tracking#track(class_488, class_80)
      */
     void track(class_488 entityTracker, class_80 trackedEntities);
 
     /**
-     * Invokes {@link CustomTracking#track(class_488, class_80)} in entity if it's instance of {@link CustomTracking}.
+     * Invokes {@link CustomTracking#track(class_488, class_80)} in entity if it's instance of {@link CustomTracking} via {@link TrackEntity} hook.
      * @param entityTracker the dimension's tracker instance.
      * @param trackedEntities the set of tracked entities.
      * @param entityToTrack the entity to invoke the method on.
