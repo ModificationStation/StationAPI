65662232375f48a12533d08e495beed9a7d4f615
mineLdiver <aabesedin@mail.ru>
2021-01-13 00:26:16 +0500

    Apparently, forgot the GuiHelper javadoc.

--- src/main/java/net/modificationstation/stationapi/api/common/gui/GuiHelper.java
+++ src/main/java/net/modificationstation/stationapi/api/common/gui/GuiHelper.java
@@ -17,11 +17,36 @@ public class GuiHelper {
 
     private static final GuiHelperImpl INSTANCE = SideUtils.get(GuiHelperClientImpl::new, GuiHelperServerImpl::new);
 
+    /**
+     * Handles side dependent container GUI.
+     * If executed on client, opens GUI, if executed on server, sends a message to client with all data to open it.
+     * Doesn't require any side-checks. If the current game is multiplayer but the code runs on client, it'll be ignored.
+     * @param player the player to open GUI for.
+     * @param identifier GUI handler identifier.
+     * @param inventory the inventory to open (usually a tile entity).
+     * @param container the container to open.
+     * @see net.modificationstation.stationapi.api.client.event.gui.GuiHandlerRegister
+     * @see GuiHandlerRegistry
+     * @see GuiHelper#openGUI(PlayerBase, Identifier, InventoryBase, ContainerBase, Consumer)
+     */
     @API
     public static void openGUI(PlayerBase player, Identifier identifier, InventoryBase inventory, ContainerBase container) {
         INSTANCE.openGUI(player, identifier, inventory, container);
     }
 
+    /**
+     * Handles side dependent container GUI with additional data via customData consumer.
+     * If executed on client, opens GUI, if executed on server, sends a message to client with all data to open it.
+     * Doesn't require any side-checks. If the current game is multiplayer but the code runs on client, it'll be ignored.
+     * @param player the player to open GUI for.
+     * @param identifier GUI handler identifier.
+     * @param inventory the inventory to open (usually a tile entity).
+     * @param container the container to open.
+     * @param customData the packet consumer that can add additional data to the GUI packet.
+     * @see net.modificationstation.stationapi.api.client.event.gui.GuiHandlerRegister
+     * @see GuiHandlerRegistry
+     * @see GuiHelper#openGUI(PlayerBase, Identifier, InventoryBase, ContainerBase)
+     */
     @API
     public static void openGUI(PlayerBase player, Identifier identifier, InventoryBase inventory, ContainerBase container, Consumer<Message> customData) {
         INSTANCE.openGUI(player, identifier, inventory, container, customData);
