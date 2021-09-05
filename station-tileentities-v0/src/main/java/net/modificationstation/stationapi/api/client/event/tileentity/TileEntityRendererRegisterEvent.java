a3310d257e66c012692da11c07bb45fcf261d99e
mineLdiver <aabesedin@mail.ru>
2021-09-05 16:06:41 +0500

    Refactor PR #13

--- station-tileentities-v0/src/main/java/net/modificationstation/stationapi/api/client/event/tileentity/TileEntityRendererRegisterEvent.java
+++ station-tileentities-v0/src/main/java/net/modificationstation/stationapi/api/client/event/tileentity/TileEntityRendererRegisterEvent.java
@@ -1,14 +1,15 @@
-package net.modificationstation.stationapi.api.event.tileentity;
+package net.modificationstation.stationapi.api.client.event.tileentity;
 
 import lombok.RequiredArgsConstructor;
 import net.mine_diver.unsafeevents.Event;
 import net.minecraft.client.render.tileentity.TileEntityRenderer;
 import net.minecraft.tileentity.TileEntityBase;
 
-import java.util.Map;
+import java.util.*;
 
 @RequiredArgsConstructor
 public class TileEntityRendererRegisterEvent extends Event {
+
     public final Map<Class<? extends TileEntityBase>, TileEntityRenderer> renderers;
 
     @Override
--- station-tileentities-v0/src/main/java/net/modificationstation/stationapi/mixin/tileentity/client/MixinTileEntityRenderDispatcher.java
+++ station-tileentities-v0/src/main/java/net/modificationstation/stationapi/mixin/tileentity/client/MixinTileEntityRenderDispatcher.java
@@ -1,20 +1,21 @@
-package net.modificationstation.stationapi.mixin.tileentity;
+package net.modificationstation.stationapi.mixin.tileentity.client;
 
 import net.minecraft.client.render.entity.TileEntityRenderDispatcher;
 import net.minecraft.client.render.tileentity.TileEntityRenderer;
 import net.minecraft.tileentity.TileEntityBase;
 import net.modificationstation.stationapi.api.StationAPI;
-import net.modificationstation.stationapi.api.event.tileentity.TileEntityRendererRegisterEvent;
+import net.modificationstation.stationapi.api.client.event.tileentity.TileEntityRendererRegisterEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.util.Map;
+import java.util.*;
 
 @Mixin(TileEntityRenderDispatcher.class)
 public class MixinTileEntityRenderDispatcher {
-    @SuppressWarnings({"unchecked"})
-    @Redirect(method = "<init>()V", at = @At(value = "INVOKE", target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"))
+
+    @SuppressWarnings({"unchecked", "UnresolvedMixinReference"})
+    @Redirect(method = "<init>()V", at = @At(value = "INVOKE", target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;", ordinal = 2))
     private <K, V> V initCustomRenderers(Map<K, V> map, K key, V value){
         V ret = map.put(key, value);
         StationAPI.EVENT_BUS.post(new TileEntityRendererRegisterEvent((Map<Class<? extends TileEntityBase>, TileEntityRenderer>) map));
