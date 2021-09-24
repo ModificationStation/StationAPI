289adf59ff5673afaba9f9c0f3372555d6f629d5
mineLdiver <aabesedin@mail.ru>
2021-09-24 19:06:20 +0500

    Removed ModelRegisterEvent and made render-api client-only module.

--- station-render-api-v0/src/main/java/net/modificationstation/stationapi/mixin/render/MixinBlockBase.java
+++ station-render-api-v0/src/main/java/net/modificationstation/stationapi/mixin/render/MixinBlockBase.java
@@ -1,27 +1,13 @@
 package net.modificationstation.stationapi.mixin.render;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.minecraft.block.BlockBase;
-import net.modificationstation.stationapi.api.StationAPI;
-import net.modificationstation.stationapi.api.client.event.model.ModelRegisterEvent;
 import net.modificationstation.stationapi.api.client.texture.atlas.Atlas;
 import net.modificationstation.stationapi.api.client.texture.atlas.Atlases;
 import net.modificationstation.stationapi.api.client.texture.atlas.CustomAtlasProvider;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BlockBase.class)
 public class MixinBlockBase implements CustomAtlasProvider {
-
-    @SuppressWarnings("UnresolvedMixinReference")
-    @Environment(EnvType.CLIENT)
-    @Inject(method = "<clinit>", at = @At(value = "NEW", target = "(II)Lnet/minecraft/block/Stone;", ordinal = 0, shift = At.Shift.BEFORE))
-    private static void beforeBlockRegister(CallbackInfo ci) {
-        StationAPI.EVENT_BUS.post(new ModelRegisterEvent(ModelRegisterEvent.Type.BLOCKS));
-    }
 
     @Override
     public Atlas getAtlas() {
--- station-render-api-v0/src/main/java/net/modificationstation/stationapi/mixin/render/MixinItemBase.java
+++ station-render-api-v0/src/main/java/net/modificationstation/stationapi/mixin/render/MixinItemBase.java
@@ -1,27 +1,13 @@
 package net.modificationstation.stationapi.mixin.render;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.minecraft.item.ItemBase;
-import net.modificationstation.stationapi.api.StationAPI;
-import net.modificationstation.stationapi.api.client.event.model.ModelRegisterEvent;
 import net.modificationstation.stationapi.api.client.texture.atlas.Atlas;
 import net.modificationstation.stationapi.api.client.texture.atlas.Atlases;
 import net.modificationstation.stationapi.api.client.texture.atlas.CustomAtlasProvider;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemBase.class)
 public class MixinItemBase implements CustomAtlasProvider {
-
-    @Environment(EnvType.CLIENT)
-    @SuppressWarnings("UnresolvedMixinReference")
-    @Inject(method = "<clinit>", at = @At(value = "NEW", target = "(ILnet/minecraft/item/tool/ToolMaterial;)Lnet/minecraft/item/tool/Shovel;", ordinal = 0, shift = At.Shift.BEFORE))
-    private static void beforeItemRegister(CallbackInfo ci) {
-        StationAPI.EVENT_BUS.post(new ModelRegisterEvent(ModelRegisterEvent.Type.ITEMS));
-    }
 
     @Override
     public Atlas getAtlas() {
