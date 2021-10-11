a534e3844cd97aeef27f7d22184e679f9c656a47
mineLdiver <aabesedin@mail.ru>
2021-10-11 20:46:08 +0500

    Added BlockSet event to level-events to allow mods to listen to any block placement.

--- station-level-events-v0/src/main/java/net/modificationstation/stationapi/api/event/level/LevelEvent.java
+++ station-level-events-v0/src/main/java/net/modificationstation/stationapi/api/event/level/LevelEvent.java
@@ -1,9 +1,11 @@
 package net.modificationstation.stationapi.api.event.level;
 
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import net.mine_diver.unsafeevents.Event;
 import net.minecraft.level.Level;
+import net.minecraft.level.chunk.Chunk;
 
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class LevelEvent extends Event {
@@ -14,6 +16,38 @@ public abstract class LevelEvent extends Event {
 
         public Init(Level level) {
             super(level);
+        }
+
+        @Override
+        protected int getEventID() {
+            return ID;
+        }
+
+        public static final int ID = NEXT_ID.incrementAndGet();
+    }
+
+    public static class BlockSet extends LevelEvent {
+        
+        @Getter
+        private final boolean cancellable = true;
+
+        public final Chunk chunk;
+        public final int
+                x, y, z,
+                blockId, blockMeta;
+
+        public BlockSet(Level level, Chunk chunk, int x, int y, int z, int blockId) {
+            this(level, chunk, x, y, z, blockId, 0);
+        }
+
+        public BlockSet(Level level, Chunk chunk, int x, int y, int z, int blockId, int blockMeta) {
+            super(level);
+            this.chunk = chunk;
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.blockId = blockId;
+            this.blockMeta = blockMeta;
         }
 
         @Override
--- station-level-events-v0/src/main/java/net/modificationstation/stationapi/mixin/level/MixinLevel.java
+++ station-level-events-v0/src/main/java/net/modificationstation/stationapi/mixin/level/MixinLevel.java
@@ -1,12 +1,15 @@
 package net.modificationstation.stationapi.mixin.level;
 
 import net.minecraft.level.Level;
+import net.minecraft.level.chunk.Chunk;
 import net.modificationstation.stationapi.api.StationAPI;
 import net.modificationstation.stationapi.api.event.level.LevelEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(Level.class)
 public class MixinLevel {
@@ -21,5 +24,37 @@ public class MixinLevel {
     )
     private void onCor1(CallbackInfo ci) {
         StationAPI.EVENT_BUS.post(new LevelEvent.Init((Level) (Object) this));
+    }
+
+    @Inject(
+            method = "setTileWithMetadata(IIIII)Z",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/level/Level;getChunkFromCache(II)Lnet/minecraft/level/chunk/Chunk;",
+                    shift = At.Shift.BY,
+                    by = 2
+            ),
+            locals = LocalCapture.CAPTURE_FAILHARD,
+            cancellable = true
+    )
+    private void onBlockSetWithMeta(int x, int y, int z, int blockId, int blockMeta, CallbackInfoReturnable<Boolean> cir, Chunk chunk) {
+        if (StationAPI.EVENT_BUS.post(new LevelEvent.BlockSet((Level) (Object) this, chunk, x, y, z, blockId, blockMeta)).isCancelled())
+            cir.setReturnValue(false);
+    }
+
+    @Inject(
+            method = "setTileInChunk(IIII)Z",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/level/Level;getChunkFromCache(II)Lnet/minecraft/level/chunk/Chunk;",
+                    shift = At.Shift.BY,
+                    by = 2
+            ),
+            locals = LocalCapture.CAPTURE_FAILHARD,
+            cancellable = true
+    )
+    private void onBlockSet(int x, int y, int z, int blockId, CallbackInfoReturnable<Boolean> cir, Chunk chunk) {
+        if (StationAPI.EVENT_BUS.post(new LevelEvent.BlockSet((Level) (Object) this, chunk, x, y, z, blockId)).isCancelled())
+            cir.setReturnValue(false);
     }
 }
