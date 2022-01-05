4f95164c4d7a3fa1b46b5a5cbc1a39a673542f69
mineLdiver <aabesedin@mail.ru>
2022-01-05 14:46:32 +0500

    Revert again

--- station-templates-v0/src/main/java/net/modificationstation/stationapi/api/template/block/BlockTemplate.java
+++ station-templates-v0/src/main/java/net/modificationstation/stationapi/api/template/block/BlockTemplate.java
@@ -1,17 +1,24 @@
 package net.modificationstation.stationapi.api.template.block;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.EnvironmentInterface;
 import net.minecraft.block.BlockBase;
 import net.minecraft.item.ItemBase;
 import net.modificationstation.stationapi.api.block.BlockItemToggle;
 import net.modificationstation.stationapi.api.block.StationFlatteningBlock;
 import net.modificationstation.stationapi.api.block.StationItemsBlock;
+import net.modificationstation.stationapi.api.client.texture.atlas.Atlas;
+import net.modificationstation.stationapi.api.client.texture.atlas.CustomAtlasProvider;
 import net.modificationstation.stationapi.api.registry.BlockRegistry;
 import net.modificationstation.stationapi.api.registry.Identifier;
 import net.modificationstation.stationapi.api.registry.ModID;
 import net.modificationstation.stationapi.api.registry.Registry;
 import net.modificationstation.stationapi.api.util.Util;
 
+@EnvironmentInterface(value = EnvType.CLIENT, itf = CustomAtlasProvider.class)
 public interface BlockTemplate<T extends BlockBase> extends
+        CustomAtlasProvider,
         StationItemsBlock,
         BlockItemToggle<T>,
         StationFlatteningBlock
@@ -20,6 +27,12 @@ public interface BlockTemplate<T extends BlockBase> extends
     default T setTranslationKey(ModID modID, String translationKey) {
         //noinspection unchecked
         return (T) ((BlockBase) this).setTranslationKey(Identifier.of(modID, translationKey).toString());
+    }
+
+    @Override
+    @Environment(EnvType.CLIENT)
+    default Atlas getAtlas() {
+        return Util.assertImpl();
     }
 
     @Override
