9143782c93fbe168b0fe09745a7bdf190653547c
mineLdiver <aabesedin@mail.ru>
2022-04-28 01:29:29 +0500

    Render terrain in regions (fixes transparent block rendering, slightly increases performance) Water requires a fix (only renders on both coords positive quad of the world)

--- station-renderer-arsenic/src/main/java/net/modificationstation/stationapi/mixin/arsenic/client/MixinWorldRenderer.java
+++ station-renderer-arsenic/src/main/java/net/modificationstation/stationapi/mixin/arsenic/client/MixinWorldRenderer.java
@@ -1,190 +1,144 @@
 package net.modificationstation.stationapi.mixin.arsenic.client;
 
 import net.minecraft.class_66;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.render.RenderList;
 import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.entity.player.PlayerBase;
+import net.minecraft.entity.Living;
 import net.modificationstation.stationapi.api.client.blaze3d.systems.RenderSystem;
-import net.modificationstation.stationapi.api.client.gl.GlUniform;
 import net.modificationstation.stationapi.api.client.gl.VertexBuffer;
-import net.modificationstation.stationapi.api.client.render.BufferRenderer;
-import net.modificationstation.stationapi.api.client.render.RenderLayer;
-import net.modificationstation.stationapi.api.client.render.Shader;
-import net.modificationstation.stationapi.api.client.render.VertexFormat;
-import net.modificationstation.stationapi.api.util.math.Vec3f;
+import net.modificationstation.stationapi.api.client.render.*;
 import net.modificationstation.stationapi.impl.client.arsenic.renderer.render.ChunkBuilderVBO;
 import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-
-import java.util.List;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(WorldRenderer.class)
 public abstract class MixinWorldRenderer {
 
-    @Shadow private Minecraft client;
-
-    @Shadow private class_66[] field_1808;
-
-    @Shadow private boolean field_1817;
-
-    @Shadow private List<class_66> field_1793;
-
-    @Shadow private int field_1787;
-
-    @Shadow private int field_1791;
-
-    @Shadow private int field_1788;
-
-    @Shadow private int field_1789;
-
-    @Shadow private int field_1790;
-
     @Shadow private RenderList[] field_1794;
 
-    /**
-     * @reason early version
-     * @author mine_diver
-     */
-    @Overwrite
-    private int method_1542(int i, int j, int k, double d) {
-        int n;
-        this.field_1793.clear();
-        int n2 = 0;
-        for (int i2 = i; i2 < j; ++i2) {
-            if (k == 0) {
-                ++this.field_1787;
-                if (this.field_1808[i2].field_244[k]) {
-                    ++this.field_1791;
-                } else if (!this.field_1808[i2].field_243) {
-                    ++this.field_1788;
-                } else if (this.field_1817 && !this.field_1808[i2].field_252) {
-                    ++this.field_1789;
-                } else {
-                    ++this.field_1790;
-                }
-            }
-            if (this.field_1808[i2].field_244[k] || !this.field_1808[i2].field_243 || this.field_1817 && !this.field_1808[i2].field_252 || this.field_1808[i2].method_297(k) < 0) continue;
-            this.field_1793.add(this.field_1808[i2]);
-            ++n2;
-        }
-        PlayerBase living = (PlayerBase) this.client.viewEntity;
-        double d2 = living.prevRenderX + (living.x - living.prevRenderX) * d;
-        double d3 = living.prevRenderY + (living.y - living.prevRenderY) * d;
-        double d4 = living.prevRenderZ + (living.z - living.prevRenderZ) * d;
-        int n4 = 0;
-        for (n = 0; n < this.field_1794.length; ++n) {
-            this.field_1794[n].method_1913();
-        }
+    @SuppressWarnings({"InvalidMemberReference", "UnresolvedMixinReference", "MixinAnnotationTarget", "InvalidInjectorMethodSignature"})
+    @Redirect(
+            method = "<init>(Lnet/minecraft/client/Minecraft;Lnet/minecraft/client/texture/TextureManager;)V",
+            at = @At(
+                    value = "NEW",
+                    target = "()Lnet/minecraft/client/render/RenderList;"
+            )
+    )
+    private RenderList injectRenderRegion() {
+        return new RenderRegion();
+    }
 
+    @Inject(
+            method = "method_1542(IIID)I",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/render/RenderList;method_1910(I)V",
+                    shift = At.Shift.BEFORE
+            ),
+            locals = LocalCapture.CAPTURE_FAILHARD
+    )
+    private void addBufferToRegion(int j, int k, int d, double par4, CallbackInfoReturnable<Integer> cir, int var6, Living var7, double var8, double var10, double var12, int var14, int var15, class_66 var16, int var17) {
+        ((RenderRegion) this.field_1794[var17]).addBuffer(((ChunkBuilderVBO) var16).getBuffers().get(d == 0 ? RenderLayer.getSolid() : RenderLayer.getTranslucent()));
+    }
+
+    @Redirect(
+            method = "method_1542(IIID)I",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/render/RenderList;method_1910(I)V"
+            )
+    )
+    private void stopCallingRenderList(RenderList instance, int i) {}
+
+    @Inject(
+            method = "method_1540(ID)V",
+            at = @At("HEAD")
+    )
+    public void beforeRenderRegion(int d, double par2, CallbackInfo ci) {
         RenderSystem.assertOnRenderThread();
-        RenderLayer layer = k == 0 ? RenderLayer.getSolid() : RenderLayer.getTranslucent();
+        RenderLayer layer = d == 0 ? RenderLayer.getSolid() : RenderLayer.getTranslucent();
         layer.startDrawing();
-        VertexFormat format = layer.getVertexFormat();
         Shader shader = RenderSystem.getShader();
         BufferRenderer.unbindAll();
 
-        for(int shaderTex = 0; shaderTex < 12; ++shaderTex) {
-            int l = RenderSystem.getShaderTexture(shaderTex);
-            shader.addSampler("Sampler" + shaderTex, l);
-        }
-
-        if (shader.modelViewMat != null) {
-            shader.modelViewMat.set(RenderSystem.getModelViewMatrix());
-        }
-
-        if (shader.projectionMat != null) {
-            shader.projectionMat.set(RenderSystem.getProjectionMatrix());
-        }
-
-        if (shader.colorModulator != null) {
-            shader.colorModulator.set(RenderSystem.getShaderColor());
-        }
-
-        if (shader.fogMode != null) {
-            shader.fogMode.set(RenderSystem.getShaderFogMode().getId());
-        }
-
-        if (shader.fogDensity != null) {
-            shader.fogDensity.set(RenderSystem.getShaderFogDensity());
-        }
-
-        if (shader.fogStart != null) {
-            shader.fogStart.set(RenderSystem.getShaderFogStart());
-        }
-
-        if (shader.fogEnd != null) {
-            shader.fogEnd.set(RenderSystem.getShaderFogEnd());
-        }
-
-        if (shader.fogColor != null) {
-            shader.fogColor.set(RenderSystem.getShaderFogColor());
-        }
-
-        if (shader.fogShape != null) {
-            shader.fogShape.set(RenderSystem.getShaderFogShape().getId());
-        }
-
-        if (shader.textureMat != null) {
-            shader.textureMat.set(RenderSystem.getTextureMatrix());
-        }
-
-        if (shader.gameTime != null) {
-            shader.gameTime.set(RenderSystem.getShaderGameTime());
-        }
-
-        RenderSystem.setupShaderLights(shader);
-        shader.bind();
-        GlUniform glUniform = shader.chunkOffset;
-
-        boolean rendered = false;
-        for (n = 0; n < this.field_1793.size(); ++n) {
-            class_66 class_662 = this.field_1793.get(n);
-            int n5 = -1;
-            for (int i3 = 0; i3 < n4; ++i3) {
-                if (!this.field_1794[i3].method_1911(class_662.field_237, class_662.field_238, class_662.field_239)) continue;
-                n5 = i3;
+        if (shader != null) {
+            for (int shaderTex = 0; shaderTex < 12; ++shaderTex) {
+                int l = RenderSystem.getShaderTexture(shaderTex);
+                shader.addSampler("Sampler" + shaderTex, l);
             }
-            if (n5 < 0) {
-                n5 = n4++;
-                this.field_1794[n5].method_1912(class_662.field_237, class_662.field_238, class_662.field_239, d2, d3, d4);
+
+            if (shader.modelViewMat != null) {
+                shader.modelViewMat.set(RenderSystem.getModelViewMatrix());
             }
-            RenderListAccessor renderList = (RenderListAccessor) this.field_1794[n5];
-            VertexBuffer vbo = ((ChunkBuilderVBO) class_662).getBuffers().get(layer);
-            if (glUniform != null) {
-                glUniform.set(renderList.stationapi$getField_2480() - renderList.stationapi$getField_2483(), renderList.stationapi$getField_2481() - renderList.stationapi$getField_2484(), renderList.stationapi$getField_2482() - renderList.stationapi$getField_2485());
-                glUniform.upload();
+
+            if (shader.projectionMat != null) {
+                shader.projectionMat.set(RenderSystem.getProjectionMatrix());
             }
-            vbo.drawVertices();
-            rendered = true;
-//            this.field_1794[n5].method_1910(class_662.method_297(k));
+
+            if (shader.colorModulator != null) {
+                shader.colorModulator.set(RenderSystem.getShaderColor());
+            }
+
+            if (shader.fogMode != null) {
+                shader.fogMode.set(RenderSystem.getShaderFogMode().getId());
+            }
+
+            if (shader.fogDensity != null) {
+                shader.fogDensity.set(RenderSystem.getShaderFogDensity());
+            }
+
+            if (shader.fogStart != null) {
+                shader.fogStart.set(RenderSystem.getShaderFogStart());
+            }
+
+            if (shader.fogEnd != null) {
+                shader.fogEnd.set(RenderSystem.getShaderFogEnd());
+            }
+
+            if (shader.fogColor != null) {
+                shader.fogColor.set(RenderSystem.getShaderFogColor());
+            }
+
+            if (shader.fogShape != null) {
+                shader.fogShape.set(RenderSystem.getShaderFogShape().getId());
+            }
+
+            if (shader.textureMat != null) {
+                shader.textureMat.set(RenderSystem.getTextureMatrix());
+            }
+
+            if (shader.gameTime != null) {
+                shader.gameTime.set(RenderSystem.getShaderGameTime());
+            }
+
+            RenderSystem.setupShaderLights(shader);
+            shader.bind();
+        }
+    }
+
+    @Inject(
+            method = "method_1540(ID)V",
+            at = @At("RETURN")
+    )
+    public void afterRenderRegion(int d, double par2, CallbackInfo ci) {
+        RenderLayer layer = d == 0 ? RenderLayer.getSolid() : RenderLayer.getTranslucent();
+        VertexFormat format = layer.getVertexFormat();
+        Shader shader = RenderSystem.getShader();
+        if (shader != null) {
+            shader.unbind();
         }
 
-        if (glUniform != null) {
-            glUniform.set(Vec3f.ZERO);
-        }
-
-        shader.unbind();
-
-        if (rendered) {
-            format.endDrawing();
-        }
+        format.endDrawing();
         VertexBuffer.unbind();
         VertexBuffer.unbindVertexArray();
         layer.endDrawing();
-        this.method_1540(k, d);
         GL11.glEnable(2929);
-        return n2;
-    }
-
-    /**
-     * @reason early version
-     * @author mine_diver
-     */
-    @Overwrite
-    public void method_1540(int i, double d) {
-
     }
 }
