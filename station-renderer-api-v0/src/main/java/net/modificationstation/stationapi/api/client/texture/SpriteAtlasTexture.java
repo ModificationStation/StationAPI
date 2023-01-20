e9bb3e85e99ab228aff317cfdec1173e312b25a0
mineLdiver <aabesedin@mail.ru>
2023-01-20 15:42:46 +0500

    Revert "Well well"
    This reverts commit 9e5e354f20372fa80edf838849feed46d09fdb61.

--- station-renderer-api-v0/src/main/java/net/modificationstation/stationapi/api/client/texture/SpriteAtlasTexture.java
+++ station-renderer-api-v0/src/main/java/net/modificationstation/stationapi/api/client/texture/SpriteAtlasTexture.java
@@ -1,42 +1,17 @@
 package net.modificationstation.stationapi.api.client.texture;
 
-import it.unimi.dsi.fastutil.objects.ReferenceOpenHashSet;
-import it.unimi.dsi.fastutil.objects.ReferenceSet;
 import net.minecraft.client.texture.TextureManager;
 import net.modificationstation.stationapi.api.registry.Identifier;
-import net.modificationstation.stationapi.api.resource.FakeResourceManager;
 import net.modificationstation.stationapi.api.resource.ResourceHelper;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.Set;
 
 public class SpriteAtlasTexture {
 
     private final Identifier id;
-    private final String path;
-    private BufferedImage image;
-    private final ReferenceSet<Identifier> spritesToLoad = new ReferenceOpenHashSet<>();
+    private final int glId;
 
     public SpriteAtlasTexture(TextureManager manager, Identifier id) {
         this.id = id;
-        path = ResourceHelper.ASSETS.toPath(id, "textures/atlas", "png");
-        FakeResourceManager.registerProvider(s -> {
-            if (path.equals(s)) {
-                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-                try {
-                    ImageIO.write(image, "png", outputStream);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                return new ByteArrayInputStream(outputStream.toByteArray());
-            } else
-                return null;
-        });
-    }
+        glId = manager.getTextureId(ResourceHelper.ASSETS.toPath(id, "textures/atlas", "png"));
 
-    public
+    }
 }
--- station-renderer-api-v0/src/main/java/net/modificationstation/stationapi/impl/client/render/StationRendererImpl.java
+++ station-renderer-api-v0/src/main/java/net/modificationstation/stationapi/impl/client/render/StationRendererImpl.java
@@ -1,22 +1,11 @@
 package net.modificationstation.stationapi.impl.client.render;
 
 import net.mine_diver.unsafeevents.listener.EventListener;
-import net.modificationstation.stationapi.api.StationAPI;
-import net.modificationstation.stationapi.api.client.event.resource.ClientResourceReloaderRegisterEvent;
 import net.modificationstation.stationapi.api.client.event.resource.ClientResourcesReloadEvent;
 import net.modificationstation.stationapi.api.mod.entrypoint.Entrypoint;
 import net.modificationstation.stationapi.api.mod.entrypoint.EventBusPolicy;
-import net.modificationstation.stationapi.api.registry.Identifier;
-import net.modificationstation.stationapi.api.resource.FakeResourceManager;
-import net.modificationstation.stationapi.api.resource.ResourceHelper;
 import net.modificationstation.stationapi.api.util.Null;
 import org.apache.logging.log4j.Logger;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 
 @Entrypoint(eventBus = @EventBusPolicy(registerInstance = false))
 public final class StationRendererImpl {
@@ -27,41 +16,5 @@ public final class StationRendererImpl {
     @EventListener(numPriority = Integer.MAX_VALUE / 2 + Integer.MAX_VALUE / 4)
     private static void resourceReload(ClientResourcesReloadEvent event) {
 
-    }
-
-    @EventListener(numPriority = Integer.MAX_VALUE / 2 + Integer.MAX_VALUE / 4)
-    private static void registerResourceReloaders(ClientResourceReloaderRegisterEvent event) {
-        FakeResourceManager.registerProvider(path -> {
-            try {
-                Identifier identifier = ResourceHelper.ASSETS.toId(path, StationAPI.MODID + "/textures", "png");
-                if (TERRAIN.slicedSpritesheetView.containsKey(identifier)) {
-                    BufferedImage image = TERRAIN.slicedSpritesheetView.get(identifier);
-                    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-                    try {
-                        ImageIO.write(image, "png", outputStream);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                    return new ByteArrayInputStream(outputStream.toByteArray());
-                }
-            } catch (IllegalArgumentException ignored) {}
-            return null;
-        });
-        FakeResourceManager.registerProvider(path -> {
-            try {
-                Identifier identifier = ResourceHelper.ASSETS.toId(path, StationAPI.MODID + "/textures", "png");
-                if (GUI_ITEMS.slicedSpritesheetView.containsKey(identifier)) {
-                    BufferedImage image = GUI_ITEMS.slicedSpritesheetView.get(identifier);
-                    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-                    try {
-                        ImageIO.write(image, "png", outputStream);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                    return new ByteArrayInputStream(outputStream.toByteArray());
-                }
-            } catch (IllegalArgumentException ignored) {}
-            return null;
-        });
     }
 }
