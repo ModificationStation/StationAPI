97c7cd5566c24f0750751c125d38e4dca4824598
calmilamsy <bumbill00@gmail.com>
2023-02-07 17:20:51 +0000

    Fix right clicking on sheep with anything but shears crashing the game

--- station-tools-api-v1/src/main/java/net/modificationstation/stationapi/impl/item/HijackShearsImplV1.java
+++ station-tools-api-v1/src/main/java/net/modificationstation/stationapi/impl/item/HijackShearsImplV1.java
@@ -10,6 +10,7 @@ public class HijackShearsImplV1 {
     //TODO: Make this match anything that has shear tool properties. Not sure how to go around this at the moment.
     @EventListener(priority = ListenerPriority.HIGH)
     private static void hijackShearsEvent(ShearsOverrideEvent event) {
-        event.overrideShears = !event.overrideShears && ((ToolLevel) event.itemStack.getType()).getEffectiveBlocks(event.itemStack).id().equals(Identifier.of("mineable/shears"));
+        if(!event.overrideShears && event.itemStack.getType() instanceof ToolLevel)
+            event.overrideShears = ((ToolLevel) event.itemStack.getType()).getEffectiveBlocks(event.itemStack).id().equals(Identifier.of("mineable/shears"));
     }
 }
