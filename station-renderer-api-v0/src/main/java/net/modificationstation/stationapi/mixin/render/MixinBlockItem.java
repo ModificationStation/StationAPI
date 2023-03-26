c399c5dd648bce18369da511592e79d284ca3c25
paulevsGitch <paulevs@yandex.ru>
2023-03-26 19:35:00 +0300

    Fix crash in prod

--- station-renderer-api-v0/src/main/java/net/modificationstation/stationapi/mixin/render/MixinBlockItem.java
+++ station-renderer-api-v0/src/main/java/net/modificationstation/stationapi/mixin/render/MixinBlockItem.java
@@ -2,16 +2,20 @@ package net.modificationstation.stationapi.mixin.render;
 
 import net.minecraft.block.BlockBase;
 import net.minecraft.item.Block;
+import net.minecraft.item.ItemBase;
 import net.modificationstation.stationapi.api.client.texture.atlas.Atlas;
 import net.modificationstation.stationapi.api.client.texture.atlas.CustomAtlasProvider;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(Block.class)
-public class MixinBlock implements CustomAtlasProvider {
-
+public class MixinBlockItem extends ItemBase implements CustomAtlasProvider {
     @Shadow private int blockId;
-
+    
+    protected MixinBlockItem(int i) {
+        super(i);
+    }
+    
     @Override
     public Atlas getAtlas() {
         return ((CustomAtlasProvider) BlockBase.BY_ID[blockId]).getAtlas();
