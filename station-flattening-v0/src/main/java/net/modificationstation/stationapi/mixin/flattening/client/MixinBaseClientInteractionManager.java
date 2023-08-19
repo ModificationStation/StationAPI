2270e9d2d28050bc29dc6ebf2f5ae99a70dc6917
paulevsGitch <paulevs@yandex.ru>
2023-08-20 01:16:59 +0300

    Added missing client mixin

---
+++ station-flattening-v0/src/main/java/net/modificationstation/stationapi/mixin/flattening/client/MixinBaseClientInteractionManager.java
@@ -0,0 +1,25 @@
+package net.modificationstation.stationapi.mixin.flattening.client;
+
+import net.minecraft.client.BaseClientInteractionManager;
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+@Mixin(BaseClientInteractionManager.class)
+public class MixinBaseClientInteractionManager {
+	@Shadow @Final protected Minecraft minecraft;
+	
+	@ModifyArgs(method = "method_1716", at = @At(
+		value = "INVOKE",
+		target = "Lnet/minecraft/level/Level;playLevelEvent(IIIII)V"
+	))
+	private void changeIDStorage(Args args) {
+		int data = minecraft.level.getTileId(args.get(2), args.get(3), args.get(4)) << 4;
+		data |= minecraft.level.getTileMeta(args.get(2), args.get(3), args.get(4)) & 15;
+		args.set(5, data);
+	}
+}
