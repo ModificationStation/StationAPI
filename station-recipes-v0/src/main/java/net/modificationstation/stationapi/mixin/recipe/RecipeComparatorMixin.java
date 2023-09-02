792b332aeb6ea8d7dcd01c481da297f15cfe59b1
mineLdiver <aabesedin@mail.ru>
2023-09-03 01:16:46 +0500

    Vanilla mixins broke me

--- station-recipes-v0/src/main/java/net/modificationstation/stationapi/mixin/recipe/RecipeComparatorMixin.java
+++ station-recipes-v0/src/main/java/net/modificationstation/stationapi/mixin/recipe/RecipeComparatorMixin.java
@@ -2,21 +2,18 @@ package net.modificationstation.stationapi.mixin.recipe;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.recipe.ShapedRecipe;
+import net.minecraft.recipe.ShapelessRecipe;
 import net.modificationstation.stationapi.impl.recipe.StationShapedRecipe;
 import net.modificationstation.stationapi.impl.recipe.StationShapelessRecipe;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 
 @Mixin(targets = "net.minecraft.recipe.RecipeRegistry$1")
 public class RecipeComparatorMixin {
     @WrapOperation(
             method = "method_543",
-            at = @At(
-                    value = "CONSTANT",
-                    opcode = Opcodes.INSTANCEOF,
-                    args = "classValue=net/minecraft/recipe/ShapelessRecipe"
-            )
+            constant = @Constant(classValue = ShapelessRecipe.class)
     )
     private boolean stationapi_accountForStationShapelessRecipe(Object recipe, Operation<Boolean> otherConditions) {
         return recipe instanceof StationShapelessRecipe || otherConditions.call(recipe);
@@ -24,11 +21,7 @@ public class RecipeComparatorMixin {
 
     @WrapOperation(
             method = "method_543",
-            at = @At(
-                    value = "CONSTANT",
-                    opcode = Opcodes.INSTANCEOF,
-                    args = "classValue=net/minecraft/recipe/ShapedRecipe"
-            )
+            constant = @Constant(classValue = ShapedRecipe.class)
     )
     private boolean stationapi_accountForStationShapedRecipe(Object recipe, Operation<Boolean> otherConditions) {
         return recipe instanceof StationShapedRecipe || otherConditions.call(recipe);
