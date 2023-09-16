64be127e77405f3519b87863f89c7f4df7e35888
Daniel Fildán <fildand@centrum.cz>
2023-09-16 12:13:45 +0200

    Corrected mixin name

--- station-items-v0/src/main/java/net/modificationstation/stationapi/mixin/item/client/MixinSinglePlayerClientInteractionManager.java
+++ station-items-v0/src/main/java/net/modificationstation/stationapi/mixin/item/client/MixinSinglePlayerClientInteractionManager.java
@@ -3,18 +3,20 @@ package net.modificationstation.stationapi.mixin.item.client;
 import net.minecraft.client.BaseClientInteractionManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.SinglePlayerClientInteractionManager;
+import net.minecraft.item.ItemInstance;
 import net.minecraft.util.hit.HitType;
 import net.modificationstation.stationapi.api.StationAPI;
 import net.modificationstation.stationapi.api.event.entity.player.PlayerEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(SinglePlayerClientInteractionManager.class)
-public class MixinRemoteClientInteractionManager extends BaseClientInteractionManager {
+public class MixinSinglePlayerClientInteractionManager extends BaseClientInteractionManager {
 
-    public MixinRemoteClientInteractionManager(Minecraft minecraft) {
+    public MixinSinglePlayerClientInteractionManager(Minecraft minecraft) {
         super(minecraft);
     }
 
@@ -27,5 +29,15 @@ public class MixinRemoteClientInteractionManager extends BaseClientInteractionMa
                         .currentReach(cir.getReturnValueF())
                         .build()
         ).currentReach);
+    }
+
+    @Inject(method = "method_1721", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockBase;getHardness(Lnet/minecraft/entity/player/PlayerBase;)F"), cancellable = true)
+    public void method_1721_preMine(int x, int y, int z, int par4, CallbackInfo ci){
+        ItemInstance itemInstance = this.minecraft.player.inventory.getHeldItem();
+        if(itemInstance != null){
+            if(!itemInstance.preMine(x,y,z,par4,this.minecraft.player)){
+                ci.cancel();
+            }
+        }
     }
 }
