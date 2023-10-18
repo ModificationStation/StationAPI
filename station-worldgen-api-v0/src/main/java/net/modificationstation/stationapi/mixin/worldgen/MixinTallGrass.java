6e7528a7a3e2a6104cb3e406a5ef704041e7dc47
paulevsGitch <paulevs@yandex.ru>
2023-10-18 12:33:38 +0300

    Random grass color (restored vanilla behaviour)

--- station-worldgen-api-v0/src/main/java/net/modificationstation/stationapi/mixin/worldgen/MixinTallGrass.java
+++ station-worldgen-api-v0/src/main/java/net/modificationstation/stationapi/mixin/worldgen/MixinTallGrass.java
@@ -17,6 +17,11 @@ public class MixinTallGrass {
     )
     private void getBiomeColor(BlockView view, int x, int y, int z, CallbackInfoReturnable<Integer> info) {
         if (view.getTileMeta(x, y, z) != 0) {
+            long l = x * 3129871L + z * 6129781L + y;
+            l = l * l * 42317861L + l * 11L;
+            x += (int) (l >> 14) & 7;
+            z += (int) (l >> 24) & 7;
+            
             int color = BiomeColorsImpl.GRASS_INTERPOLATOR.getColor(view.getBiomeSource(), x, z);
             info.setReturnValue(color);
         }
