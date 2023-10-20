313615342e496f1ff5c9f6ecc06ba4eb6a6ef928
paulevsGitch <paulevs@yandex.ru>
2023-10-20 08:00:29 +0300

    Fix for client-server crash related to null biome source

--- station-worldgen-api-v0/src/main/java/net/modificationstation/stationapi/mixin/worldgen/MixinLevel.java
+++ station-worldgen-api-v0/src/main/java/net/modificationstation/stationapi/mixin/worldgen/MixinLevel.java
@@ -1,5 +1,7 @@
 package net.modificationstation.stationapi.mixin.worldgen;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.level.Level;
 import net.minecraft.level.dimension.Dimension;
 import net.minecraft.level.dimension.DimensionData;
@@ -13,14 +15,40 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Level.class)
 public class MixinLevel {
     @Inject(
-            method = "<init>(Lnet/minecraft/level/dimension/DimensionData;Ljava/lang/String;JLnet/minecraft/level/dimension/Dimension;)V",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/level/dimension/Dimension;initDimension(Lnet/minecraft/level/Level;)V",
-                    shift = Shift.BEFORE
-            )
+        method = "<init>(Lnet/minecraft/level/dimension/DimensionData;Ljava/lang/String;JLnet/minecraft/level/dimension/Dimension;)V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/level/dimension/Dimension;initDimension(Lnet/minecraft/level/Level;)V",
+            shift = Shift.BEFORE
+        )
     )
     private void onInit(DimensionData data, String name, long seed, Dimension dimension, CallbackInfo info) {
         BiomeAPI.init(seed);
+    }
+    
+    @Inject(
+        method = "<init>(Lnet/minecraft/level/dimension/DimensionData;Ljava/lang/String;Lnet/minecraft/level/dimension/Dimension;J)V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/level/dimension/Dimension;initDimension(Lnet/minecraft/level/Level;)V",
+            shift = Shift.BEFORE
+        )
+    )
+    @Environment(value= EnvType.CLIENT)
+    private void onInit(DimensionData data, String name, Dimension dimension, long seed, CallbackInfo info) {
+        BiomeAPI.init(seed);
+    }
+    
+    @Inject(
+        method = "<init>(Lnet/minecraft/level/Level;Lnet/minecraft/level/dimension/Dimension;)V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/level/dimension/Dimension;initDimension(Lnet/minecraft/level/Level;)V",
+            shift = Shift.BEFORE
+        )
+    )
+    @Environment(value= EnvType.CLIENT)
+    private void onInit(Level level, Dimension dimension, CallbackInfo info) {
+        BiomeAPI.init(level.getSeed());
     }
 }
