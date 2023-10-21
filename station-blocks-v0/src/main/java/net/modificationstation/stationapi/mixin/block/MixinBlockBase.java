3110ea32c5fe8149a9fd7b3fe13ffe5295eb79ac
paulevsGitch <paulevs@yandex.ru>
2023-10-21 17:16:04 +0300

    Small optimisation

--- station-blocks-v0/src/main/java/net/modificationstation/stationapi/mixin/block/MixinBlockBase.java
+++ station-blocks-v0/src/main/java/net/modificationstation/stationapi/mixin/block/MixinBlockBase.java
@@ -3,6 +3,7 @@ package net.modificationstation.stationapi.mixin.block;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.block.BlockBase;
+import net.minecraft.block.material.Material;
 import net.modificationstation.stationapi.api.block.BlockState;
 import net.modificationstation.stationapi.api.block.StationBlock;
 import net.modificationstation.stationapi.api.registry.Identifier;
@@ -13,9 +14,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.function.Function;
 import java.util.function.ToIntFunction;
 
 @Mixin(BlockBase.class)
@@ -23,7 +25,7 @@ public abstract class MixinBlockBase implements StationBlock {
     @Shadow public abstract BlockBase setTranslationKey(String string);
     @Shadow @Final public int id;
     
-    @Unique private ToIntFunction<BlockState> emittanceProvider;
+    @Unique private ToIntFunction<BlockState> emittanceProvider = state -> BlockBase.EMITTANCE[state.getBlock().id];
     
     @Override
     public BlockBase setTranslationKey(ModID modID, String translationKey) {
@@ -47,10 +49,7 @@ public abstract class MixinBlockBase implements StationBlock {
     
     @Override
     public int getEmittance(BlockState state) {
-        if (emittanceProvider != null) {
-            return emittanceProvider.applyAsInt(state);
-        }
-        return BlockBase.EMITTANCE[state.getBlock().id];
+        return emittanceProvider.applyAsInt(state);
     }
     
     @Environment(value= EnvType.CLIENT)
