4193406017b5ab4e8dbc40933d9729ef669084ce
mineLdiver <aabesedin@mail.ru>
2023-10-30 17:24:44 +0500

    Reindented code. Fixed missing station-maths-v0 dependency

--- station-api-base/src/main/java/net/modificationstation/stationapi/api/util/collection/WeightedList.java
+++ station-api-base/src/main/java/net/modificationstation/stationapi/api/util/collection/WeightedList.java
@@ -8,28 +8,28 @@ import java.util.List;
 import java.util.Random;
 
 public class WeightedList<T> {
-	private final List<T> objects = new ArrayList<>();
-	private final IntList weights = new IntArrayList();
-	private int maxWeight;
-	
-	public void add(T object, int weight) {
-		objects.add(object);
-		maxWeight += weight;
-		weights.add(maxWeight);
-	}
-	
-	public void clear() {
-		objects.clear();
-		weights.clear();
-		maxWeight = 0;
-	}
-	
-	public T get(Random random) {
-		if (maxWeight == 0 || objects.isEmpty()) return null;
-		int weight = random.nextInt(maxWeight);
-		for (int i = 0; i < objects.size(); i++) {
-			if (weight < weights.getInt(i)) return objects.get(i);
-		}
-		return objects.get(0);
-	}
+    private final List<T> objects = new ArrayList<>();
+    private final IntList weights = new IntArrayList();
+    private int maxWeight;
+
+    public void add(T object, int weight) {
+        objects.add(object);
+        maxWeight += weight;
+        weights.add(maxWeight);
+    }
+
+    public void clear() {
+        objects.clear();
+        weights.clear();
+        maxWeight = 0;
+    }
+
+    public T get(Random random) {
+        if (maxWeight == 0 || objects.isEmpty()) return null;
+        int weight = random.nextInt(maxWeight);
+        for (int i = 0; i < objects.size(); i++) {
+            if (weight < weights.getInt(i)) return objects.get(i);
+        }
+        return objects.get(0);
+    }
 }
--- station-items-v0/src/main/java/net/modificationstation/stationapi/mixin/item/MixinDye.java
+++ station-items-v0/src/main/java/net/modificationstation/stationapi/mixin/item/MixinDye.java
@@ -13,23 +13,23 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Dye.class)
 public class MixinDye {
-	@Inject(method = "useOnTile", at = @At(
-		value = "INVOKE",
-		target = "Lnet/minecraft/level/Level;getTileId(III)I",
-		ordinal = 0
-	), cancellable = true)
-	private void onBonemealUse(ItemInstance item, PlayerBase player, Level level, int x, int y, int z, int side, CallbackInfoReturnable<Boolean> info) {
-		BlockState state = level.getBlockState(x, y, z);
-		if (state.getBlock().onBonemealUse(level, x, y, z, state)) {
-			level.method_202(x, y, z, x, y, z);
-			info.setReturnValue(true);
-			item.count--;
-			return;
-		}
-		if (BonemealAPI.generate(level, x, y, z, state, side)) {
-			level.method_202(x - 8, y - 8, z - 8, x + 8, y + 8, z + 8);
-			info.setReturnValue(true);
-			item.count--;
-		}
-	}
+    @Inject(method = "useOnTile", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/level/Level;getTileId(III)I",
+            ordinal = 0
+    ), cancellable = true)
+    private void onBonemealUse(ItemInstance item, PlayerBase player, Level level, int x, int y, int z, int side, CallbackInfoReturnable<Boolean> info) {
+        BlockState state = level.getBlockState(x, y, z);
+        if (state.getBlock().onBonemealUse(level, x, y, z, state)) {
+            level.method_202(x, y, z, x, y, z);
+            info.setReturnValue(true);
+            item.count--;
+            return;
+        }
+        if (BonemealAPI.generate(level, x, y, z, state, side)) {
+            level.method_202(x - 8, y - 8, z - 8, x + 8, y + 8, z + 8);
+            info.setReturnValue(true);
+            item.count--;
+        }
+    }
 }
