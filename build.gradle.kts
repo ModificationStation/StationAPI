2114188102a4eb96f83e43f35fe2f7b3be895d74
calmilamsy <bumbill00@gmail.com>
2024-07-11 20:36:08 +0100

    GRADLE LET THE PROJECT GET REPLACED BY A TRANSITIVE DEPENDENCY?!

--- build.gradle.kts
+++ build.gradle.kts
@@ -96,10 +96,13 @@ allprojects {
         }
 
 
-        modImplementation("net.glasslauncher.mods:GlassConfigAPI:${project.properties["gcapi_version"]}")
-        modLocalRuntime("net.glasslauncher:HowManyItems-Fabric-Unofficial:${project.properties["hmi_version"]}") {
+        implementation("me.carleslc:Simple-Yaml:1.8.4")
+        modImplementation("net.glasslauncher.mods:GlassConfigAPI:${project.properties["gcapi_version"]}") {
             isTransitive = false
         }
+//        modLocalRuntime("net.glasslauncher:HowManyItems-Fabric-Unofficial:${project.properties["hmi_version"]}") {
+//            isTransitive = false
+//        }
         // Optional bugfix mod for testing qol. Remove the // to enable.
         //modLocalRuntime "maven.modrinth:mojangfix:${project.properties["mojangfix_version"]}"
     }
