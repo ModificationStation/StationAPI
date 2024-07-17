187780b86b4af1e2c685788d38a2f1570a4d3f41
Matthew Periut <matthewperiut@gmail.com>
2024-07-17 15:37:53 -0400

    generate and cache box[] lines once per block observation

--- station-blocks-v0/src/main/java/net/modificationstation/stationapi/mixin/block/client/WorldRendererMixin.java
+++ station-blocks-v0/src/main/java/net/modificationstation/stationapi/mixin/block/client/WorldRendererMixin.java
@@ -8,12 +8,14 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.hit.HitResultType;
 import net.minecraft.world.World;
+import net.modificationstation.stationapi.api.block.BlockState;
 import net.modificationstation.stationapi.api.block.HasVoxelShape;
 import net.modificationstation.stationapi.api.util.math.Vec3d;
 import net.modificationstation.stationapi.impl.block.Line;
 import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -26,9 +28,17 @@ import static net.modificationstation.stationapi.impl.block.BoxToLinesConverter.
 public abstract class WorldRendererMixin {
     @Shadow private World world;
 
+    @Unique
+    Block lastBlock = null;
+    @Unique
+    HitResult lastHitResult = null;
+    @Unique
+    List<Line> lines = null;
+
     @Inject(method = "method_1554(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/hit/HitResult;ILnet/minecraft/item/ItemStack;F)V", at = @At("HEAD"), cancellable = true)
     private void stationapi_drawBlockVoxelShapesOutline(PlayerEntity playerEntity, HitResult hitResult, int i, ItemStack itemStack, float f, CallbackInfo ci) {
-        if (i == 0 && hitResult.type == HitResultType.BLOCK && world.getBlockState(hitResult.blockX, hitResult.blockY, hitResult.blockZ).getBlock() instanceof HasVoxelShape block) {
+        BlockState blockState = world.getBlockState(hitResult.blockX, hitResult.blockY, hitResult.blockZ);
+        if (i == 0 && hitResult.type == HitResultType.BLOCK && blockState.getBlock() instanceof HasVoxelShape block) {
             GL11.glEnable(3042);
             GL11.glBlendFunc(770, 771);
             GL11.glColor4f(0.0F, 0.0F, 0.0F, 0.4F);
@@ -44,16 +54,21 @@ public abstract class WorldRendererMixin {
                 double interpolatedY = playerEntity.field_1638 + (playerEntity.y - playerEntity.field_1638) * (double)f;
                 double interpolatedZ = playerEntity.field_1639 + (playerEntity.z - playerEntity.field_1639) * (double)f;
 
-                Vec3d center = new Vec3d(hitResult.blockX + 0.5, hitResult.blockY + 0.5, hitResult.blockZ + 0.5);
-                List<Line> linePoints = convertBoxesToLines(block.getVoxelShape(world, hitResult.blockX, hitResult.blockY, hitResult.blockZ), center);
+                if (!((lastBlock != null && ((lastBlock.id == blockState.getBlock().id) && lastHitResult.blockX == hitResult.blockX && lastHitResult.blockY == hitResult.blockY && lastHitResult.blockZ == hitResult.blockZ)))) {
+                    Vec3d center = new Vec3d(hitResult.blockX + 0.5, hitResult.blockY + 0.5, hitResult.blockZ + 0.5);
+                    lines = convertBoxesToLines(block.getVoxelShape(world, hitResult.blockX, hitResult.blockY, hitResult.blockZ), center);
+                }
 
                 Tessellator tessellator = Tessellator.INSTANCE;
                 tessellator.start(1);
-                for (Line linePoint : linePoints) {
-                    tessellator.vertex(-interpolatedX + linePoint.points[0].x, -interpolatedY + linePoint.points[0].y, -interpolatedZ + linePoint.points[0].z);
-                    tessellator.vertex(-interpolatedX + linePoint.points[1].x, -interpolatedY + linePoint.points[1].y, -interpolatedZ + linePoint.points[1].z);
+                for (Line line : lines) {
+                    tessellator.vertex(-interpolatedX + line.points[0].x, -interpolatedY + line.points[0].y, -interpolatedZ + line.points[0].z);
+                    tessellator.vertex(-interpolatedX + line.points[1].x, -interpolatedY + line.points[1].y, -interpolatedZ + line.points[1].z);
                 }
                 tessellator.draw();
+
+                lastHitResult = hitResult;
+                lastBlock = blockState.getBlock();
             }
 
             GL11.glDepthMask(true);
@@ -62,5 +77,6 @@ public abstract class WorldRendererMixin {
 
             ci.cancel();
         }
+
     }
 }
