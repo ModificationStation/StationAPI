0501d930fb6e86aeef1edf69bf519baf446e78da
calmilamsy <bumbill00@gmail.com>
2024-08-06 20:17:44 +0100

    Mention that containers can be null for tooltip events

--- station-items-v0/src/main/java/net/modificationstation/stationapi/api/client/event/gui/screen/container/TooltipBuildEvent.java
+++ station-items-v0/src/main/java/net/modificationstation/stationapi/api/client/event/gui/screen/container/TooltipBuildEvent.java
@@ -5,11 +5,16 @@ import lombok.experimental.SuperBuilder;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.entity.player.PlayerInventory;
 import net.modificationstation.stationapi.api.event.item.ItemStackEvent;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
 
 @SuperBuilder
 public class TooltipBuildEvent extends ItemStackEvent {
+    /**
+     * Containers aren't guaranteed to exist when rendering a tooltip.
+     */
+    @Nullable
     public final HandledScreen container;
     public final PlayerInventory inventory;
     public final ArrayList<String> tooltip;
--- station-items-v0/src/main/java/net/modificationstation/stationapi/api/client/event/gui/screen/container/TooltipRenderEvent.java
+++ station-items-v0/src/main/java/net/modificationstation/stationapi/api/client/event/gui/screen/container/TooltipRenderEvent.java
@@ -8,11 +8,16 @@ import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.entity.player.PlayerInventory;
 import net.modificationstation.stationapi.api.StationAPI;
 import net.modificationstation.stationapi.api.event.item.ItemStackEvent;
+import org.jetbrains.annotations.Nullable;
 
 @Cancelable
 @SuperBuilder
 @EventPhases(StationAPI.INTERNAL_PHASE)
 public class TooltipRenderEvent extends ItemStackEvent {
+    /**
+     * Containers aren't guaranteed to exist when rendering a tooltip.
+     */
+    @Nullable
     public final HandledScreen container;
     public final TextRenderer textManager;
     public final PlayerInventory inventory;
