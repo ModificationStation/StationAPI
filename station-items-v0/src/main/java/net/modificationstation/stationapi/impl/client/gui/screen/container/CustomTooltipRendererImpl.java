bf4e97e11263f53cc22cec5ea55c30fd96503cda
mineLdiver <aabesedin@mail.ru>
2024-09-06 21:05:51 +0500

    *actually* remove deprecated stuff

--- station-items-v0/src/main/java/net/modificationstation/stationapi/impl/client/gui/screen/container/CustomTooltipRendererImpl.java
+++ station-items-v0/src/main/java/net/modificationstation/stationapi/impl/client/gui/screen/container/CustomTooltipRendererImpl.java
@@ -1,19 +1,23 @@
 package net.modificationstation.stationapi.impl.client.gui.screen.container;
 
 import net.mine_diver.unsafeevents.listener.EventListener;
+import net.minecraft.client.gui.DrawContext;
 import net.modificationstation.stationapi.api.StationAPI;
 import net.modificationstation.stationapi.api.client.TooltipHelper;
 import net.modificationstation.stationapi.api.client.event.gui.screen.container.TooltipRenderEvent;
 import net.modificationstation.stationapi.api.mod.entrypoint.Entrypoint;
 import net.modificationstation.stationapi.api.mod.entrypoint.EventBusPolicy;
 import net.modificationstation.stationapi.mixin.item.client.DrawContextAccessor;
+import net.modificationstation.stationapi.mixin.item.client.HandledScreenAccessor;
 
-import java.util.*;
+import java.util.ArrayList;
 import java.util.stream.IntStream;
 
 @Entrypoint(eventBus = @EventBusPolicy(registerInstance = false))
 @EventListener(phase = StationAPI.INTERNAL_PHASE)
 public final class CustomTooltipRendererImpl {
+    private static final DrawContext CONTEXT = new DrawContext();
+
     @EventListener
     private static void renderCustomTooltips(TooltipRenderEvent event) {
         if(event.isCanceled()) {
@@ -24,9 +28,9 @@ public final class CustomTooltipRendererImpl {
 
         if (!newTooltip.isEmpty()) {
             newTooltip.stream().mapToInt(event.textManager::getWidth).max().ifPresent(tooltipWidth -> {
-                int tooltipX = event.mouseX - event.containerX + 12;
-                int tooltipY = event.mouseY - event.containerY - 12;
-                ((DrawContextAccessor) event.container).invokeFillGradient(tooltipX - 3, tooltipY - 3, tooltipX + tooltipWidth + 3, tooltipY + (8 * newTooltip.size()) + (3 * newTooltip.size()), -1073741824, -1073741824);
+                int tooltipX = event.mouseX - (event.container == null ? 0 : ((event.container.width - ((HandledScreenAccessor) event.container).stationapi_getBackgroundWidth()) / 2)) + 12;
+                int tooltipY = event.mouseY - (event.container == null ? 0 : ((event.container.height - ((HandledScreenAccessor) event.container).stationapi_getBackgroundHeight()) / 2)) - 12;
+                ((DrawContextAccessor) CONTEXT).invokeFillGradient(tooltipX - 3, tooltipY - 3, tooltipX + tooltipWidth + 3, tooltipY + (8 * newTooltip.size()) + (3 * newTooltip.size()), -1073741824, -1073741824);
                 IntStream.range(0, newTooltip.size()).forEach(currentTooltip -> event.textManager.drawWithShadow(newTooltip.get(currentTooltip), tooltipX, tooltipY + (8 * currentTooltip) + (3 * currentTooltip), -1));
             });
             event.cancel();
