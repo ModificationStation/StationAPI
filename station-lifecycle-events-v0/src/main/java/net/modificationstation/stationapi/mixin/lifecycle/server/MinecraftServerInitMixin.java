bffce829b96143d604d2c4c779aacb187ec47089
calmilamsy <bumbill00@gmail.com>
2024-10-25 22:34:52 +0100

    Add InitFinishedEvent

---
+++ station-lifecycle-events-v0/src/main/java/net/modificationstation/stationapi/mixin/lifecycle/server/MinecraftServerInitMixin.java
@@ -0,0 +1,25 @@
+package net.modificationstation.stationapi.mixin.lifecycle.server;
+
+import net.minecraft.server.MinecraftServer;
+import net.modificationstation.stationapi.api.StationAPI;
+import net.modificationstation.stationapi.api.event.init.InitFinishedEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = MinecraftServer.class, priority = 800)
+public class MinecraftServerInitMixin {
+    @Inject(
+            method = "method_2166",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/util/logging/Logger;info(Ljava/lang/String;)V",
+                    ordinal = 3,
+                    shift = At.Shift.AFTER
+            )
+    )
+    private void stationapi_endInit(CallbackInfoReturnable<Boolean> cir) {
+        StationAPI.EVENT_BUS.post(new InitFinishedEvent());
+    }
+}
