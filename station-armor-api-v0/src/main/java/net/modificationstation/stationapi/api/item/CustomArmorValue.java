95a0ca5ef08d45db8fae28fa7d115ee7c2ff52ab
mine_diver <aabesedin@mail.ru>
2024-12-24 01:12:09 +0500

    Removed broken reimplementation of Divisor's old PlayerAPI.

--- station-armor-api-v0/src/main/java/net/modificationstation/stationapi/api/item/CustomArmorValue.java
+++
@@ -1,7 +0,0 @@
-package net.modificationstation.stationapi.api.item;
-
-import net.minecraft.entity.player.PlayerEntity;
-
-public interface CustomArmorValue {
-    double modifyDamageDealt(PlayerEntity player, int armorSlot, int initialDamage, double currentAdjustedDamage);
-}
