d389af2b8301dba9cf73964f39bf4415aa8bfa57
mine_diver <aabesedin@mail.ru>
2025-01-04 00:17:26 +0500

    Replaced fake BlockItem with null, since MixinExtras allows that, unlike vanilla Mixin's Redirect.

--- station-blockitems-v0/src/main/java/net/modificationstation/stationapi/impl/block/BlockFormOnlyHandler.java
+++ station-blockitems-v0/src/main/java/net/modificationstation/stationapi/impl/block/BlockFormOnlyHandler.java
@@ -1,18 +1,13 @@
 package net.modificationstation.stationapi.impl.block;
 
-import com.google.common.base.Suppliers;
 import net.mine_diver.unsafeevents.listener.EventListener;
-import net.minecraft.item.BlockItem;
 import net.modificationstation.stationapi.api.StationAPI;
 import net.modificationstation.stationapi.api.event.block.BlockItemFactoryEvent;
 import net.modificationstation.stationapi.api.mod.entrypoint.Entrypoint;
 import net.modificationstation.stationapi.api.mod.entrypoint.EntrypointManager;
 import net.modificationstation.stationapi.api.mod.entrypoint.EventBusPolicy;
-import net.modificationstation.stationapi.api.util.UnsafeProvider;
-import org.jetbrains.annotations.ApiStatus;
 
 import java.lang.invoke.MethodHandles;
-import java.util.function.Supplier;
 
 @Entrypoint(eventBus = @EventBusPolicy(registerInstance = false))
 @EventListener(phase = StationAPI.INTERNAL_PHASE)
@@ -20,15 +15,6 @@ public class BlockFormOnlyHandler {
     static {
         EntrypointManager.registerLookup(MethodHandles.lookup());
     }
-
-    @ApiStatus.Internal
-    public static final Supplier<BlockItem> EMPTY_BLOCK_ITEM = Suppliers.memoize(() -> {
-        try {
-            return (BlockItem) UnsafeProvider.theUnsafe.allocateInstance(BlockItem.class);
-        } catch (InstantiationException e) {
-            throw new RuntimeException(e);
-        }
-    });
 
     @EventListener
     private static void registerBlockItem(BlockItemFactoryEvent event) {
