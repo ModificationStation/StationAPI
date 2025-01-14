c8db80ad67e3f54c5b6899e145daa9bd8891b49e
mine_diver <aabesedin@mail.ru>
2025-01-15 02:09:10 +0500

    ContextVariable

---
+++ station-api-base/src/main/java/net/modificationstation/stationapi/impl/util/ContextVariable.java
@@ -0,0 +1,57 @@
+package net.modificationstation.stationapi.impl.util;
+
+import it.unimi.dsi.fastutil.Pair;
+import lombok.val;
+import org.jetbrains.annotations.ApiStatus;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * A substitute for Java's preview feature "ScopedValue".
+ * <p>
+ *     Part of the internal API, not to be used in other mods.
+ * </p>
+ */
+@ApiStatus.Internal
+public final class ContextVariable<T> {
+    public interface ContextBuilder {
+        <T> ContextBuilder where(ContextVariable<T> variable, T value);
+
+        void run(Runnable runnable);
+    }
+
+    public static <T> ContextBuilder where(ContextVariable<T> variable, T value) {
+        val builder = new ContextBuilder() {
+            private final List<Pair<Runnable, Runnable>> binds = new ArrayList<>();
+
+            @Override
+            public <V> ContextBuilder where(ContextVariable<V> variable, V value) {
+                binds.add(Pair.of(
+                        () -> variable.values.get().push(value),
+                        () -> variable.values.get().pop()
+                ));
+                return this;
+            }
+
+            @Override
+            public void run(Runnable runnable) {
+                binds.stream().map(Pair::first).forEach(Runnable::run);
+                try {
+                    runnable.run();
+                } finally {
+                    binds.stream().map(Pair::second).forEach(Runnable::run);
+                }
+            }
+        };
+        return builder.where(variable, value);
+    }
+
+    private final ThreadLocal<Deque<T>> values = ThreadLocal.withInitial(ArrayDeque::new);
+
+    public T get() {
+        return values.get().peek();
+    }
+}
