622c50ed811ac06dd0f1d5d1712e9a5a271054f6
mineLdiver <aabesedin@mail.ru>
2025-02-09 19:25:14 +0500

    Fix a NPE in shears override

--- station-tools-api-v1/src/main/java/net/modificationstation/stationapi/impl/item/HijackShearsImplV1.java
+++ station-tools-api-v1/src/main/java/net/modificationstation/stationapi/impl/item/HijackShearsImplV1.java
@@ -1,5 +1,6 @@
 package net.modificationstation.stationapi.impl.item;
 
+import lombok.val;
 import net.mine_diver.unsafeevents.listener.EventListener;
 import net.modificationstation.stationapi.api.StationAPI;
 import net.modificationstation.stationapi.api.item.tool.StationTool;
@@ -17,7 +18,10 @@ public class HijackShearsImplV1 {
     //TODO: Make this match anything that has shear tool properties. Not sure how to go around this at the moment.
     @EventListener
     private static void hijackShearsEvent(ShearsOverrideEvent event) {
-        if(!event.overrideShears && event.itemStack.getItem() instanceof StationTool)
-            event.overrideShears = ((StationTool) event.itemStack.getItem()).getEffectiveBlocks(event.itemStack).id().equals(Identifier.of("mineable/shears"));
+        if (!event.overrideShears && event.itemStack.getItem() instanceof StationTool tool) {
+            val effectiveBlocks = tool.getEffectiveBlocks(event.itemStack);
+            if (effectiveBlocks != null)
+                event.overrideShears = effectiveBlocks.id().equals(Identifier.of("mineable/shears"));
+        }
     }
 }
