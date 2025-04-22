abe354f682addcea6d79aed004c4fa1c6c659fbb
calmilamsy <bumbill00@gmail.com>
2025-04-22 16:23:41 +0100

    Fix

--- src/test/java/net/modificationstation/sltest/level/gen/ChunkListener.java
+++ src/test/java/net/modificationstation/sltest/level/gen/ChunkListener.java
@@ -1,6 +1,8 @@
 package net.modificationstation.sltest.level.gen;
 
 import net.mine_diver.unsafeevents.listener.EventListener;
+import net.minecraft.block.Block;
+import net.modificationstation.sltest.block.Blocks;
 import net.modificationstation.stationapi.api.event.world.gen.WorldGenEvent;
 
 public class ChunkListener {
@@ -11,5 +13,10 @@ public class ChunkListener {
 //            for (int z = 0; z < 16; z++)
 //                if ((event.biome == Biome.FOREST || event.biome == Biome.SEASONAL_FOREST) && event.random.nextBoolean())
 //                    event.level.setTile(event.x + x, 90, event.z + z, BlockBase.DIAMOND_BLOCK.id);
+        // Modded leaves and log test
+        event.world.setBlock(event.x, 100, event.z, Blocks.MODDED_LOG.get().id);
+        event.world.setBlock(event.x + 1, 100, event.z, Block.LEAVES.id);
+        event.world.setBlock(event.x + 2, 100, event.z, Blocks.MODDED_LEAVES.get().id);
+        event.world.setBlock(event.x + 3, 100, event.z, Block.LEAVES.id);
     }
 }
