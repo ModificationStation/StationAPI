effeb5575b7cfc75ca464765c745f1dadf64e50d
calmilamsy <bumbill00@gmail.com>
2025-05-19 16:14:48 +0100

    Oops

--- station-renderer-api-v0/src/main/java/net/modificationstation/stationapi/api/client/event/texture/TextureRegisterEvent.java
+++ station-renderer-api-v0/src/main/java/net/modificationstation/stationapi/api/client/event/texture/TextureRegisterEvent.java
@@ -2,9 +2,6 @@ package net.modificationstation.stationapi.api.client.event.texture;
 
 import lombok.experimental.SuperBuilder;
 import net.mine_diver.unsafeevents.Event;
-import net.mine_diver.unsafeevents.event.EventPhases;
-import net.modificationstation.stationapi.api.StationAPI;
 
 @SuperBuilder
-@EventPhases(StationAPI.INTERNAL_PHASE)
 public class TextureRegisterEvent extends Event {}
