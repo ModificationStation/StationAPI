4a7a5ef242053f8f8b65460b354facd8c05a9f98
mineLdiver <aabesedin@mail.ru>
2025-06-17 22:11:23 +0500

    Use the appropriate random

--- station-items-v0/src/main/java/net/modificationstation/stationapi/mixin/item/SaplingBlockMixin.java
+++ station-items-v0/src/main/java/net/modificationstation/stationapi/mixin/item/SaplingBlockMixin.java
@@ -6,21 +6,17 @@ import net.modificationstation.stationapi.api.block.BlockState;
 import net.modificationstation.stationapi.api.block.StationBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 
 import java.util.Random;
 
 @Mixin(SaplingBlock.class)
 public abstract class SaplingBlockMixin implements StationBlock {
-    @Unique
-    private static final Random RANDOM = new Random();
-
     @Shadow public abstract void method_533(World world, int x, int y, int z, Random random);
 
     @Override
     public boolean onBonemealUse(World world, int x, int y, int z, BlockState state) {
         if (!world.isRemote) {
-            method_533(world, x, y, z, RANDOM);
+            method_533(world, x, y, z, world.field_214);
         }
         return true;
     }
