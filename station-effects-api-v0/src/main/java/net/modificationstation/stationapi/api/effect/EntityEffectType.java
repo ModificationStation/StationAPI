81f6002c26079438675205726bdb0701ec7e4a6d
mineLdiver <arsbsdn@gmail.com>
2025-07-27 00:24:43 +0500

    Docs

--- station-effects-api-v0/src/main/java/net/modificationstation/stationapi/api/effect/EntityEffectType.java
+++ station-effects-api-v0/src/main/java/net/modificationstation/stationapi/api/effect/EntityEffectType.java
@@ -2,17 +2,35 @@ package net.modificationstation.stationapi.api.effect;
 
 import net.modificationstation.stationapi.api.registry.RegistryEntry;
 
+/**
+ * Static reference of an effect. Since {@link EntityEffect} is initialized per-entity, a separate class is required
+ * for cases when an effect needs to be referenced in an entity-less context, such as
+ * {@link net.minecraft.entity.Entity#addEffect(EntityEffectType, int)}
+ *
+ * @param <EFFECT_INSTANCE> the class of the effect's per-entity instance.
+ */
 public final class EntityEffectType<EFFECT_INSTANCE extends EntityEffect<EFFECT_INSTANCE>> {
     @SuppressWarnings("unchecked")
     public final RegistryEntry.Reference<EntityEffectType<EFFECT_INSTANCE>> registryEntry =
             (RegistryEntry.Reference<EntityEffectType<EFFECT_INSTANCE>>) (RegistryEntry.Reference<?>)
                     EntityEffectTypeRegistry.INSTANCE.createEntry(this);
+    /**
+     * The effect instance's factory which takes an {@link net.minecraft.entity.Entity} argument of the entity
+     * the effect's been applied to, and an int argument defining the duration of the effect in ticks.
+     */
     public final EntityEffect.Factory<EFFECT_INSTANCE> factory;
 
     private EntityEffectType(Builder<EFFECT_INSTANCE> builder) {
         factory = builder.factory;
     }
 
+    /**
+     * @param factory the effect instance's factory which takes an {@link net.minecraft.entity.Entity} argument
+     *                of the entity the effect's been applied to, and an int argument defining the duration
+     *                of the effect in ticks.
+     * @return an effect type's builder instance.
+     * @param <EFFECT_INSTANCE> the class of the effect's per-entity instance.
+     */
     public static <EFFECT_INSTANCE extends EntityEffect<EFFECT_INSTANCE>> Builder<EFFECT_INSTANCE> builder(
             EntityEffect.Factory<EFFECT_INSTANCE> factory
     ) {
@@ -26,6 +44,9 @@ public final class EntityEffectType<EFFECT_INSTANCE extends EntityEffect<EFFECT_
             this.factory = factory;
         }
 
+        /**
+         * @return the built entity type.
+         */
         public EntityEffectType<EFFECT_INSTANCE> build() {
             return new EntityEffectType<>(this);
         }
--- station-effects-api-v0/src/main/java/net/modificationstation/stationapi/api/event/effect/EntityEffectTypeRegistryEvent.java
+++ station-effects-api-v0/src/main/java/net/modificationstation/stationapi/api/event/effect/EntityEffectTypeRegistryEvent.java
@@ -4,8 +4,12 @@ import net.mine_diver.unsafeevents.event.EventPhases;
 import net.modificationstation.stationapi.api.StationAPI;
 import net.modificationstation.stationapi.api.effect.EntityEffectType;
 import net.modificationstation.stationapi.api.effect.EntityEffectTypeRegistry;
+import net.modificationstation.stationapi.api.event.registry.AfterBlockAndItemRegisterEvent;
 import net.modificationstation.stationapi.api.event.registry.RegistryEvent;
 
+/**
+ * Posted during {@link AfterBlockAndItemRegisterEvent}
+ */
 @EventPhases(StationAPI.INTERNAL_PHASE)
 public class EntityEffectTypeRegistryEvent extends RegistryEvent.EntryTypeBound<EntityEffectType<?>, EntityEffectTypeRegistry> {
     public EntityEffectTypeRegistryEvent() {
