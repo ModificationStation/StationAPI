13c798e53bee9b9ee42cc67f2462373cef61de5b
mine_diver <arsbsdn@gmail.com>
2025-09-16 19:18:19 +0500

    Refactor

--- station-flattening-v0/src/main/java/net/modificationstation/stationapi/mixin/flattening/MapItemMixin.java
+++ station-flattening-v0/src/main/java/net/modificationstation/stationapi/mixin/flattening/MapItemMixin.java
@@ -1,15 +1,21 @@
 package net.modificationstation.stationapi.mixin.flattening;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.block.Block;
 import net.minecraft.item.MapItem;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(MapItem.class)
-public class MapItemMixin {
-    @ModifyConstant(method = "update", constant = @Constant(intValue = 256))
-    private int adjustArray(int constant) {
+class MapItemMixin {
+    @ModifyExpressionValue(
+            method = "update",
+            at = @At(
+                    value = "CONSTANT",
+                    args = "intValue=256"
+            )
+    )
+    private int stationapi_adjustArray(int constant) {
         return Block.BLOCKS.length;
     }
 }
