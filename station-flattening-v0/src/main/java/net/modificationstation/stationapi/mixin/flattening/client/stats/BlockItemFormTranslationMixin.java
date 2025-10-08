9ba3119fa009a26224399c0ef127a9951a224f7d
mine_diver <arsbsdn@gmail.com>
2025-10-08 07:32:10 +0500

    Cache for stats screen

--- station-flattening-v0/src/main/java/net/modificationstation/stationapi/mixin/flattening/client/stats/BlockItemFormTranslationMixin.java
+++ station-flattening-v0/src/main/java/net/modificationstation/stationapi/mixin/flattening/client/stats/BlockItemFormTranslationMixin.java
@@ -2,10 +2,14 @@ package net.modificationstation.stationapi.mixin.flattening.client.stats;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import it.unimi.dsi.fastutil.objects.Reference2IntMap;
+import it.unimi.dsi.fastutil.objects.Reference2IntOpenHashMap;
 import net.minecraft.block.Block;
 import net.minecraft.stat.ItemOrBlockStat;
 import net.minecraft.stat.Stats;
+import net.modificationstation.stationapi.api.util.Util;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(targets = {
@@ -13,6 +17,12 @@ import org.spongepowered.asm.mixin.injection.At;
         "net.minecraft.client.gui.screen.StatsScreen$BlockStatsListWidget"
 })
 class BlockItemFormTranslationMixin {
+    @Unique
+    private final Reference2IntMap<ItemOrBlockStat> stationapi_idCache = Util.make(
+            new Reference2IntOpenHashMap<>(),
+            map -> map.defaultReturnValue(-1)
+    );
+
     @WrapOperation(
             method = "*",
             at = @At(
@@ -22,15 +32,18 @@ class BlockItemFormTranslationMixin {
             require = 0
     )
     private int stationapi_translateToBlockItemForm(ItemOrBlockStat instance, Operation<Integer> original) {
+        var cachedId = stationapi_idCache.getInt(instance);
+        if (cachedId > -1)
+            return cachedId;
+
         int id = original.call(instance);
-        if (!Stats.BLOCK_MINED_STATS.contains(instance))
-            return id;
+        if (Stats.BLOCK_MINED_STATS.contains(instance)) {
+            var item = Block.BLOCKS[id].asItem();
+            if (item != null)
+                id = item.id;
+        }
 
-        var item = Block.BLOCKS[id].asItem();
-        if (item == null)
-            return id;
-
-        return item.id;
+        stationapi_idCache.put(instance, id);
+        return id;
     }
-
 }
