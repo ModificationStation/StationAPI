7aac38c199fb1846e8644cc4bed4df6e5c226065
Atilist <71230342+Atilist@users.noreply.github.com>
2026-08-17 23:23:23 +0200

    Remove MetaIntervalToTag (not worth implementing)

--- station-vanilla-fix-v0/src/main/java/net/modificationstation/stationapi/api/vanillafix/util/MetaIntervalToTag.java
+++
@@ -1,6 +0,0 @@
-package net.modificationstation.stationapi.api.vanillafix.util;
-
-import net.minecraft.nbt.NbtCompound;
-
-public record MetaIntervalToTag(int start, int end, NbtCompound tag, int outputMeta) {
-}
