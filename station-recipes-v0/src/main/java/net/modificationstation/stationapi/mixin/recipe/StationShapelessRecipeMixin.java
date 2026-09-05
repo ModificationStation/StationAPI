e1612ca709ecf3d888267ef8bf069b208e67db0f
Daniel Fildán <fildand@centrum.cz>
2026-09-05 22:30:47 +0200

    fix ami compat using the powers of Aromantic Succubus Metamorphosis

---
+++ station-recipes-v0/src/main/java/net/modificationstation/stationapi/mixin/recipe/StationShapelessRecipeMixin.java
@@ -0,0 +1,9 @@
+package net.modificationstation.stationapi.mixin.recipe;
+
+import net.modificationstation.stationapi.impl.recipe.StationShapelessRecipe;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(StationShapelessRecipe.class)
+public abstract class StationShapelessRecipeMixin {
+    // This empty mixin needs to be here
+}
