0594805d64337a35c46c70f4da25a834a1b9ad9f
mine_diver <arsbsdn@gmail.com>
2026-09-06 00:06:42 +0500

    Experimental because template managed packet is weird

--- station-networking-v0/src/main/java/net/modificationstation/stationapi/api/network/packet/template/TemplateManagedPacket.java
+++ station-networking-v0/src/main/java/net/modificationstation/stationapi/api/network/packet/template/TemplateManagedPacket.java
@@ -3,6 +3,7 @@ package net.modificationstation.stationapi.api.network.packet.template;
 import net.minecraft.network.packet.Packet;
 import net.modificationstation.stationapi.api.network.packet.ManagedPacket;
 import net.modificationstation.stationapi.impl.network.stream.TrackingOutputStream;
+import org.jetbrains.annotations.ApiStatus;
 
 import java.io.DataOutputStream;
 
@@ -10,6 +11,7 @@ import java.io.DataOutputStream;
  * A simple packet template that implements ManagedPacket and a simple size tracker so you don't have to do it yourself.
  * @param <P> Your packet class.
  */
+@ApiStatus.Experimental
 public abstract class TemplateManagedPacket<P extends Packet & ManagedPacket<P>> extends Packet implements ManagedPacket<P> {
     private static final TrackingOutputStream TRACKER = new TrackingOutputStream();
 
